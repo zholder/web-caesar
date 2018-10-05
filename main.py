3d19e360d375b95a9bd3685f7ab6713e917ab93d
Zach Holder <zacharysholder@gmail.com>
2018-10-04 22:30:36 -0500

    added form input to populate encrypted message

--- main.py
+++ main.py
@@ -10,26 +10,26 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
       <form method="POST">
             <label>Rotate by:</label>
             <input type="text" name="rot" value="0"/>
-            <textarea name="text"></textarea>
+            <textarea name="text">{0}</textarea>
             <input type="submit"/>
         </form>
     </body>
@@ -39,13 +39,13 @@ form = """
 
 @app.route("/")
 def index():
-    return form
+    return form.format('')
 
 @app.route("/", methods=['POST'])
 def encrypt():
     rot = int(request.form['rot'])
     text = request.form['text']
     rotated_string = rotate_string(text, rot)
-    return '<h1>' + rotated_string +'</h1>'
+    return '<h1>' + form.format(rotated_string) +'</h1>'
     
 app.run()
